79967a2db1cf656408c53395edcb2a55daf8579e
Alana Barrett-Frew <alanabarrett-frew@alanas-laptop.home>
2023-03-06 13:45:35 +0000

    form added using emailjs

--- reactportfolio/src/Pages/PortfolioContact/PortfolioContact.js
+++ reactportfolio/src/Pages/PortfolioContact/PortfolioContact.js
@@ -3,15 +3,18 @@ import "./style.css";
 import Button from 'react-bootstrap/Button';
 import { LinkContainer } from "react-router-bootstrap";
 import Nav from 'react-bootstrap/Nav';
+import ContactMe from "../../components/ContactForm";
 
 function PortfolioContact() {
     return (
         <div>
             <h1>Contact Me</h1>
             <p>
-                If you want to get in contact please use the following links:
-            </p>
-            <Button className="button" href="mailto: alanabarrett-frew@hotmail.com">Send me an Email</Button>
+                If you want to get in contact please use the form below or click on one of the links:
+            </p> 
+            <div>
+            <ContactMe />
+        </div>
             <Button className="button" href="https://twitter.com/AlanaBF81">Twitter</Button>
             <Button className="button" href="https://github.com/AlanaBF">Github</Button>
             <Button className="button" href="https://www.linkedin.com/in/alanabarrettfrew">LinkedIn</Button>
@@ -19,6 +22,9 @@ function PortfolioContact() {
                 If you like what you see and have a role that I may be suitable for, please take a look at my CV and LinkedIn Profile. I am open to work and would relish the challenge of a Front End Developer role.
             </p>
             <LinkContainer to="/CV"><Nav.Link ><Button className="link-button">Alana CV</Button></Nav.Link></LinkContainer>
+       
+        
+        
         </div>
     )
 }
